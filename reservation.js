803b8a2853f9a515e8884f182b1aa27316821029
Jai-Maurya-5735 <145703465+Jai-Maurya-5735@users.noreply.github.com>
2023-09-21 13:44:40 -0400

    Update reservation.js
    Coded a statement that moves the focus to the “Pickup Date” text box

--- reservation.js
+++ reservation.js
@@ -2,5 +2,7 @@
 
 $(document).ready( () => {
 	const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
-		
+	  
+	  //  Moved focus to the Pickup Date text box
+ 	  $("#pickup_date").focus();	
 }); // end ready
